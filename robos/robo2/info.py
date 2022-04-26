00a84c7913a8fcbf54ce24ed5b86def16808eb1b
og3da <rapha488@gmail.com>
2022-04-26 14:57:45 -0300

    informações sobre o robô

---
+++ robos/robo2/info.py
@@ -0,0 +1,3 @@
+class data:
+    info = 'F:/Programmer/Desktop/Robos/dados/sites.xls'
+    cDriver = 'F:/Programmer/Documentos/CURSOS/UDEMY/RobosPython_AutomatizandoProcessos/dados/chromedriver.exe'
