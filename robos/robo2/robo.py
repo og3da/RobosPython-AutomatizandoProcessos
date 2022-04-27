7a85451cbb2f714be970c9c1897fa49bc178af94
og3da <rapha488@gmail.com>
2022-04-27 14:15:51 -0300

    algumas correções e nova funcionalidade salvar resultados em um arquivo

--- robos/robo2/robo.py
+++ robos/robo2/robo.py
@@ -19,22 +19,33 @@ numero_resultados = int(resultados.split("Aproximadamente ")[1].split(' resultad
 maximo_paginas = numero_resultados/10
 print(f'Número de páginas = {maximo_paginas}')
 
-# navegando entre as paginas (ex: da pagina 1 ate a 10)
+# navegando entre as paginas (ex: da pagina 1 ate a 10) e salvando em um arquivo de texto
+lista_resultados = []
 url_pagina = driver.find_element_by_xpath("//a[@aria-label='Page 2']").get_attribute("href")
 pagina_atual = 0
+paginas_percorrer = int(input(f"Páginas encontradas {maximo_paginas} \n Digite quantas paginas deseja percorrer: "))
 start = 10
-while pagina_atual <= 8:
-    if not pagina_atual == 0:
+while pagina_atual <= paginas_percorrer-1:
+    if pagina_atual > 1:
         url_pagina = url_pagina.replace(f'start={start}',f'start={start+10}')
         start += 10
+        driver.get(url_pagina)
+    elif pagina_atual == 1:
+        driver.get(url_pagina)
     pagina_atual += 1
-    driver.get(url_pagina)
 
     # capturando titulo e link das paginas navegadas
-    divs = driver.find_elements_by_xpath("//div[@class='g']")
+    divs = driver.find_elements_by_xpath("//div[@class='yuRUbf']")
     for div in divs:
-        nome = div.find_element_by_tag_name("span")
+        nome = div.find_element_by_tag_name("h3")
         link = div.find_element_by_tag_name("a")
         resultado = f'titulo: {nome.text}, link:{link.get_attribute("href")}'
         print(resultado)
         print()
+        lista_resultados.append(resultado)
+
+with open("resultados.txt","w") as arquivo:
+    for resultado in lista_resultados:
+        arquivo.write(f'{resultado}\n')
+    arquivo.close()
+print(f'{len(lista_resultados)} resultados encontrados do Google e salvos no arquivo')
