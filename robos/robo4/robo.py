7e4f62f083ce6fd1f4c1b3dec82f5cc12dc7bd7e
og3da <rapha488@gmail.com>
2022-05-16 21:49:00 -0300

    correções no codigo e função para buscar entre páginas; versao final

--- robos/robo4/robo.py
+++ robos/robo4/robo.py
@@ -9,11 +9,26 @@ class RoboYoutube():
         self.webdriver = webdriver.Chrome(data.cDriver)
     
     def busca(self, pesquisa):
+        videos = []
+        pagina = 1
+
         url = f"https://www.youtube.com/results?search_query={pesquisa}"
         self.webdriver.get(url)
-        titulos = self.webdriver.find_elements_by_xpath("//a[@id='video-title']")
-        for titulo in titulos:
-            print(f'titulo encontrado: {titulo.text}')
+
+        while pagina <= 3:
+            titulos = self.webdriver.find_elements_by_xpath("//a[@id='video-title']")
+            for titulo in titulos:
+                if titulo.text not in videos: # validação para nao haver duplicidade de titulos
+                    print(f'titulo encontrado: {titulo.text}')
+                    videos.append(titulo.text)
+            self.proxima_pagina(pagina)
+            pagina += 1 
+
+    def proxima_pagina(self, pagina):
+        print(f'Mudando para a pagina {pagina +1}')
+        bottom = pagina * 10000 
+        self.webdriver.execute_script(f"window.scrollTo(0, {bottom});") # função JS para rolar a página até o final e carregar os proximos videos
+        time.sleep(7)
 
 bot = RoboYoutube()
 pesquisa = input("Digite a pesquisa a realizar: ")
