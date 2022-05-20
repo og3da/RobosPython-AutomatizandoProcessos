e4474ba90ef05ec2965a55da9357f4ae9780950f
og3da <rapha488@gmail.com>
2022-05-20 09:03:54 -0300

    Listando grupos e escolhendo grupo alvo

--- robos/roboTelegram/TelegramBot.py
+++ robos/roboTelegram/TelegramBot.py
@@ -1,4 +1,8 @@
-import telethon # biblioteca para utilizar funções do Telegram 
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
 import time
 from config import api_id, api_hash, phone
 
@@ -8,7 +12,7 @@ class TelegramBot:
         self.api_id   = api_id
         self.api_hash = api_hash
         self.phone    = phone
-        self.client = telethon.TelegramClient(self.phone, self.api_id, self.api_hash)
+        self.client = TelegramClient(self.phone, self.api_id, self.api_hash)
         self.connect()
 
     def connect(self):
@@ -16,4 +20,32 @@ class TelegramBot:
         if not self.client.is_user_authorized():
             self.client.send_code_request(self.phone)
             self.client.sign_in(self.phone, input("Digite o código: "))
-        return True
+        return 
+
+    def get_groups(self):
+        groups = []
+
+        chats = self.client(GetDialogsRequest(
+            offset_date=None, 
+            offset_id=0, 
+            offset_peer=InputPeerEmpty(), 
+            limit=200, 
+            hash=0))
+        
+        for chat in chats.chats:
+            try:
+                if chat.megagroup == True:
+                    groups.append(chat)
+            except:
+                continue
+
+        print("Escolha um grupo")
+        i=0
+        for group in groups:
+            print(f'{i} + {group.title}')
+            i += 1
+
+        escolha = input("Digite um número: ")
+        grupo_alvo = groups[int(escolha)]
+        return grupo_alvo
+        
