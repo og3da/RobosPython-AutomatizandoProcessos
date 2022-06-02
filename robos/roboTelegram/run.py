9e65a8442d433212f1c9d418129413297e480960
og3da <rapha488@gmail.com>
2022-06-01 22:19:24 -0300

    correção para executar o programa

--- robos/roboTelegram/run.py
+++ robos/roboTelegram/run.py
@@ -1,3 +1,7 @@
 from TelegramBot import TelegramBot
 # rodar o bot 
-obj_telegram = TelegramBot()
+obj_telegram =  TelegramBot()
+
+print("Iniciando o robô...")
+grupo_alvo = obj_telegram.get_groups()
+membros = obj_telegram.get_members_group(grupo_alvo)
