b3d96fabfc5fe811d38beabc5a71c588cfcd2173
og3da <rapha488@gmail.com>
2022-06-19 15:34:37 -0300

    Adicionar membros ao grupo; versao final

--- robos/roboTelegram/run.py
+++ robos/roboTelegram/run.py
@@ -5,3 +5,13 @@ obj_telegram =  TelegramBot()
 print("Iniciando o robô...")
 grupo_alvo = obj_telegram.get_groups()
 membros = obj_telegram.get_members_group(grupo_alvo)
+print(f'{membros} membros encontrados no grupo alvo')
+
+print("")
+print("Escolha o grupo a adicionar os membros")
+meu_grupo = obj_telegram.get_groups()
+
+for membro in membros:
+    adicionado = obj_telegram.add_member_toGroup(membro, meu_grupo)
+    if adicionado:
+        print(f"membro {membro.id} adicionado")
